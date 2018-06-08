80d282c11e39dc9a7136ae664f831f0da94ce8ab
Ann1224 <podoynitsyna.anna@yoctocode.com>
2018-06-08 11:53:17 +0300

    add model/controller

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var db = require('./db');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
-
+var artistsController = require('./controllers/artists');
 var app = express();
 
 app.use(bodyParser.json());
@@ -14,68 +14,18 @@ app.get('/', function (req, res) {
 	res.send('Hello!');
 });
 
-app.get('/artists', function (req, res) {
-	res.send(artists);
-});
+app.get('/artists', artistsController.all);
 
-app.get('/artists/:id', function (req, res) {
+app.get('/artists/:id', artistsController.findById);
 
-	db.get().collection('artists').findOne({ _id: ObjectID(req.params.id) }, function(err, doc){
-		if (err) {
-			console.log(err);
-			return res.sendStatus(500);
-		}
-		res.send(doc);
-	})
-});
+app.post('/artists', artistsController.create);
 
-app.post('/artists', function (req, res) {
-	var artist = {
-		name: req.body.name
-	};
-
-	db.get().collection('artists').insert(artist, function(err, result) {
-		if (err) {
-			console.log(err);
-			return res.sendStatus(500);
-		}
-		res.send(artist);
-	})
-});
-
-
-app.put('/artists/:id', function (req, res) {
-	db.get().collection('artists').updateOne(
-		{ _id: ObjectID(req.params.id) },
-		{ name: req.body.name },
-		function (err, result) {
-			if(err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.sendStatus(200);
-	})
-});
-
-app.delete('/artists/:id', function (req, res) {
-	db.get().collection('artists').deleteOne(
-		{ _id: ObjectID(req.params.id) },
-		function (err, result) {
-			if(err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.sendStatus(200);
-	})
-});
+app.put('/artists/:id', artistsController.update);
 
 
 
-db.connect("mongodb://localhost:27017/myapi", function(err, database) {
-	if (err) {
-		return console.log(err);
-	}
-	app.listen(3012, function() {
-		console.log("API Start");
-	});
-});
+app.delete('/artists/:id', artistsController.delete);
+
+
+
+db.connect("mongodb://localhost:27017/myapi", );
